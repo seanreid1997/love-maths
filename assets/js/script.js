d156ee3eed364cdcbd294f57e9d3de52bb8172ce
seanreid1997 <reid.sean14@yahoo.com>
2022-02-16 20:44:46 +0000

    Remove division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,9 +122,3 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = 'x';
     
 }
-
-function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = '+';
-}
